8aa8a241d382df976f8b68601fa1fb3e16d807d8
Savita Katwe <savitamerwade04@gmail.com>
2024-01-19 22:50:24 +0400

    button component

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,13 @@
 import Button from "@/components/Button/Button";
 
 function LoginPage() {
-    return (
-        <div>
-            <h2>Login</h2>
-            <Button/>
-        </div>)
+  return (
+    <div>
+      <h2>Login</h2>
+      <Button variant={"primary"}>Login</Button>
+      <Button variant={"secondary"}>Create Account</Button>
+      <Button variant={"transparent"}>Create Account</Button>
+    </div>
+  );
 }
-export default LoginPage
+export default LoginPage;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,42 @@
 "use client";
-import React from "react";
+import React, { PropsWithChildren, useMemo } from "react";
 import styled from "styled-components";
 
-const StyledButton = styled.button`
-  background-color: blueviolet;
-  color: white;
+const StyledButton = styled.button<{
+  color: string;
+  textColor: string;
+  border: string;
+}>`
+  background-color: ${(props) => props.color};
+  color: ${(props) => props.textColor};
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: ${(props) => props.border};
 `;
-const Button = () => {
-  return <StyledButton>Click me</StyledButton>;
+
+interface IButtonProps {
+  variant: "primary" | "secondary" | "transparent";
+}
+const Button = ({ variant, children }: PropsWithChildren<IButtonProps>) => {
+  const buttonColor = useMemo(() => {
+    switch (variant) {
+      case "primary":
+        return { color: "#ff748d", textColor: "white", border: "none" };
+      case "secondary":
+        return { color: "white", textColor: "black", border: "1px solid" };
+      case "transparent":
+        return { color: "white", textColor: "black", border: "none" };
+    }
+  }, [variant]);
+
+  return (
+    <StyledButton
+      color={buttonColor.color}
+      textColor={buttonColor.textColor}
+      border={buttonColor.border}
+    >
+      {children}
+    </StyledButton>
+  );
 };
 export default Button;
