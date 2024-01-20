6aa93207863f00b4dee9294c93d4f7e0befb19c2
Savita Katwe <savitamerwade04@gmail.com>
2024-01-20 18:55:39 +0400

    template design

--- src/app/auth/template.tsx
+++ src/app/auth/template.tsx
@@ -1,10 +1,35 @@
-import {PropsWithChildren} from "react";
+"use client";
+import { PropsWithChildren } from "react";
+import Typography from "@/components/Typography/Typography";
+import Image from "next/image";
+import imagePic from "../../../public/Log in.png";
+import styled from "styled-components";
 
-function AuthTemplate({children}:PropsWithChildren) {
-    return <>
-        <h1>Keshav</h1>
+const StyledFlexContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+const StyledTitle = styled.div`
+  padding: 20px 50px;
+`;
+
+function AuthTemplate({ children }: PropsWithChildren) {
+  return (
+    <>
+      <StyledTitle>
+        <Typography variant={"title1"} color={"#ff748d"}>
+          TweetX
+        </Typography>
+      </StyledTitle>
+      <StyledFlexContainer>
         <div>{children}</div>
+        <div>
+          <Image src={imagePic} alt={"pic"} height={600} />
+        </div>
+      </StyledFlexContainer>
     </>
+  );
 }
 
-export default AuthTemplate
+export default AuthTemplate;
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,14 @@
+"use client";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+  body {
+    height: 100vh;
+  }
+`;
+export default GlobalStyle;
